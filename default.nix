cdf82eb77cc4931ea8f1be29433e5393a1c267f0
Jonah <jonahmail1@gmail.com>
2024-05-19 14:01:48 -0400

    Test just compile this one

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ lib, stdenv, fetchFromGitLab, autoreconfHook}:
+
+stdenv.mkDerivation rec {
+  pname = "html2text";
+  version = "2.2.3";
+
+# TODO see other github
+  src = fetchFromGitLab {
+    owner = "grobian";
+    repo = "html2text";
+    rev = "40c175152835fc41f01d89058145c3da4f62ae3a";
+    hash = "sha256-7Ch51nJ5BeRqs4PEIPnjCGk+Nm2ydgJQCtkcpihXun8=";
+  };
+
+  nativeBuildInputs = [ autoreconfHook ];
+
+  meta = {
+    description = "Convert HTML to plain text";
+    mainProgram = "html2text";
+    homepage = "https://gitlab.com/grobian/html2text";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = [ lib.maintainers.eikek ];
+  };
+}
